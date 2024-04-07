147f15f67dbfe921a18e1b0e6225245008da31f0
Mayari Merchant <mm478@duke.edu>
2024-04-07 16:41:22 -0400

    double integration attempt

--- reefwing_testing/reefwing_testing.ino
+++ reefwing_testing/reefwing_testing.ino
@@ -27,6 +27,7 @@
 #include <ReefwingAHRS.h>
 #include <ReefwingLSM9DS1.h>
 #include <BasicLinearAlgebra.h>
+#include <Time.h> 
 
 using namespace BLA;
 
@@ -37,7 +38,22 @@ ReefwingAHRS ahrs;
 bool readData = false;
 const float readThresh = 2.5;
 
+float dt, time, t_su, t_sd; 
+
 float cX, cY, cZ = 0;
+
+float prev_time = 0; 
+
+int status;
+
+int i, instances_above_thresh =  0; 
+
+int step_count; 
+
+#define z_sd_thresh 2 // double gravity for step down threshold 
+#define GRAVITY 9.81
+
+BLA::Matrix<4> accel_array[200]; // fill this in starting at t_su until receive a t_sd 
 
 void setup() {
   //  Initialise the LSM9DS1 IMU & AHRS
@@ -79,47 +95,69 @@ void setup() {
     Serial.println("LSM9DS1 IMU Not Detected.");
     while(1);
   }
+
+  imu.updateSensorData();
+  ahrs.setData(imu.data);
+  ahrs.update();
+  accel_array[0] = proj; // projec
+  time_stamp_array[0] = millis; 
+  i = 1; 
+
+
   Serial.println(" aX \t aY \t aZ ");
 }
 
 void loop() {
+
   imu.updateSensorData();
-  ahrs.setData(imu.data);
+  ahrs.setData(imu.data); //imu.data is a struct {ax,ay,az,gx,gy,gz,mx,my,mz,gTimeStamp,aTimeStamp,mTimeStamp}
   ahrs.update();
 
-//  if (millis() - previousMillis >= displayPeriod) {
-//    //  Display sensor data every displayPeriod, non-blocking.
-//    Serial.print("--> Roll: ");
-//    Serial.print(ahrs.angles.roll, 2);
-//    Serial.print("\tPitch: ");
-//    Serial.print(ahrs.angles.pitch, 2);
-//    Serial.print("\tYaw: ");
-//    Serial.print(ahrs.angles.yaw, 2);
-//    Serial.print("\tHeading: ");
-//    Serial.print(ahrs.angles.heading, 2);
-//    Serial.print("\tLoop Frequency: ");
-//    Serial.print(loopFrequency);
-//    Serial.println(" Hz");
-//
-//    loopFrequency = 0;
-//    previousMillis = millis();
+  if( imu.data.aTimeStamp <= time_stamp_array[i-1] ) continue;
+  //don't do anything if there's no new data (aka old timestamp is same as current timestamp)
+
+/*
+ if (millis() - previousMillis >= displayPeriod) {
+   //  Display sensor data every displayPeriod, non-blocking.
+   Serial.print("--> Roll: ");
+   Serial.print(ahrs.angles.roll, 2);
+   Serial.print("\tPitch: ");
+   Serial.print(ahrs.angles.pitch, 2);
+   Serial.print("\tYaw: ");
+   Serial.print(ahrs.angles.yaw, 2);
+   Serial.print("\tHeading: ");
+   Serial.print(ahrs.angles.heading, 2);
+   Serial.print("\tLoop Frequency: ");
+   Serial.print(loopFrequency);
+   Serial.println(" Hz");
+
+   loopFrequency = 0;
+   previousMillis = millis();
+ } */
+
+
+//MM 
+  // int status = detectStepDown(accel_array, i, prev_check_idx); // go back some amount within the array to see if we should detect a step_down event 
+
+//  if(millis() - previousMillis >= calculatePeriod){
+
 //  }
 
-//  Serial.print("Orientation: ");
-//  Serial.print(ahrs.angles.yaw);
-//  Serial.print(" ");
-//  Serial.print(ahrs.angles.pitch);
-//  Serial.print(" ");
-//  Serial.println(ahrs.angles.roll);
+ Serial.print("Orientation: ");
+ Serial.print(ahrs.angles.yaw);
+ Serial.print(" ");
+ Serial.print(ahrs.angles.pitch);
+ Serial.print(" ");
+ Serial.println(ahrs.angles.roll);
 
-//  Serial.print("Quaternion: ");
-//  Serial.print(ahrs.getQuaternion().q0);
-//  Serial.print(" ");
-//  Serial.print(ahrs.getQuaternion().q1);
-//  Serial.print(" ");
-//  Serial.print(ahrs.getQuaternion().q2);
-//  Serial.print(" ");
-//  Serial.println(ahrs.getQuaternion().q3);
+ Serial.print("Quaternion: ");
+ Serial.print(ahrs.getQuaternion().q0);
+ Serial.print(" ");
+ Serial.print(ahrs.getQuaternion().q1);
+ Serial.print(" ");
+ Serial.print(ahrs.getQuaternion().q2);
+ Serial.print(" ");
+ Serial.println(ahrs.getQuaternion().q3);
 
   Quaternion q = ahrs.getQuaternion();
 
@@ -131,16 +169,31 @@ void loop() {
 //  float ay = imu.data.ay - r1;
 //  float az = imu.data.az - r2;
 
-  BLA::Matrix<3> gravity = {r0, r1, r2};
+  BLA::Matrix<3> gravity = {r0, r1, r2}; // rotated gravity in device xyz coordinate basis 
   BLA::Matrix<3> accel = {imu.data.ax, imu.data.ay, imu.data.az};
 
-  float s = (~accel*gravity)(0);
+  float vertical_accel = (~accel*gravity)(0); // dot product 
 
-  for(int i = 0; i < 3; i++) {
-    gravity(i) *= s;
+  for(int j = 0; j < 3; j++) {
+    gravity(j) *= vertical_accel;
   }
 
-  BLA::Matrix<3> proj = accel - gravity;
+  BLA::Matrix<3> proj = accel - gravity; // unit g's (not m/s^2)
+
+
+
+  
+  //populate array, increment index 
+  prev_time = time; 
+  time = millis(); //?? 
+  dt = time - prev_time; 
+  accel_array[i] = proj; // projec
+  time_stamp_array = dt; 
+  i++; 
+  
+
+
+  // vector of acceleartion in the plane normal to gravity 
 
 //  Serial.print("Projection:\t");
   Serial.print(proj(0));
@@ -148,6 +201,28 @@ void loop() {
   Serial.print(proj(1));
   Serial.print('\t');
   Serial.println(proj(2));
+
+
+
+//MM 
+  //detect step-down 
+  if(proj(2) > z_sd_thresh){
+    instances_above_thresh ++; 
+  } else {
+    instances_above_thresh = 0; // not multiple instances_above_thresh in a row 
+  }
+
+  if(instances_above_thresh > 2){
+    if(accel_array[i-5][3] < z_sd_thresh){
+      Serial.print("step down!!!\n"); 
+      t_sd = millis(); 
+      instances_above_thresh = 0; 
+      float step_length = calculate_step_length(accel_array, i_su, i, t_sd, t_su); 
+    }
+  }
+
+  //detect step-up 
+
 
 //  Serial.print(r0);
 //  Serial.print('\t');
@@ -191,5 +266,47 @@ void loop() {
 //  Serial.print('\t');
 //  Serial.println(aTot);
 
+
+}
+
+// void append_accel(float ax, float ay, float az, float dt, BLA::Matrix<3> accel_array, uint32_t time_stamp_array[]){
+
+// }
+
+// int detectStepDown(BLA::Matrix<4> accel_array[], int cur_idx, int prev_check_idx){
+
+// }
+
+float calculate_step_length(BLA::Matrix<3> accel_array[], uint32_t time_stamp_array[], int i_su, int i_sd) { //, uint32_t t_sd, uint32_t t_su){
+  // float step_time = t_sd - t_su; 
+
+  // accel_array entry has device xyz acceleration values MINUS any acceleration in the true world z (gravity) axis 
+  // only acceleration in the ground xy plane, but has xyz components because it's pre-rotation (device coordinate) values 
+  
+  float cur_length = 0; 
+  float ax, ay, az;
+  float vx = 0;
+  float vy = 0; 
+  float vz = 0;
+  float px = 0;
+  float py = 0; 
+  float vz = 0;
+  float dt; // time between previous entry and current entry, in seconds 
+
+  for(int i = i_su; i < i_sd; i++){
+    ax = accel_array[i](0) * GRAVITY ; // multiply by gravity to get into m/s^2 rather than g's 
+    ay = accel_array[i](1) * GRAVITY ;
+    az = accel_array[i](2) * GRAVITY ; 
+    dt = time_stamp_array[i] / 1000.0; //in seconds instead of ms 
+    vx += ax * dt;
+    vy += ay * dt;
+    vz += az * dt;
+    px += vx * dt + 0.5 * ax * dt * dt;
+    py += vy * dt + 0.5 * ay * dt * dt; 
+    pz += vz * dt + 0.5 * az * dt * dt; 
+    cur_length += sqrt(px*px + py*py + pz*pz);
+  }
+
+  return cur_length; 
 
 }
